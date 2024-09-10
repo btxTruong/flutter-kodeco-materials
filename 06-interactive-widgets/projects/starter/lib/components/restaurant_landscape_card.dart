d1583f4c5f8d536c4e90ff58cd2140d230a03940
truongbui <btxtruong@gmail.com>
2024-09-10 22:40:49 +0700

    feat: using sliver 6. advanced scrollable widgets

--- 06-interactive-widgets/projects/starter/lib/components/restaurant_landscape_card.dart
+++ 06-interactive-widgets/projects/starter/lib/components/restaurant_landscape_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/restaurant.dart';
+import '../screens/restaurant_page.dart';
 
 class RestaurantLandscapeCard extends StatefulWidget {
   final Restaurant restaurant;
@@ -23,60 +24,69 @@ class _RestaurantLandscapeCardState extends State<RestaurantLandscapeCard> {
     final textTheme = Theme.of(context)
         .textTheme
         .apply(displayColor: Theme.of(context).colorScheme.onSurface);
-    return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.vertical(
-              top: Radius.circular(8.0),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RestaurantPage(
+              restaurant: widget.restaurant,
             ),
-            child: AspectRatio(
-              aspectRatio: 2,
-              child: Stack(
-                fit: StackFit.expand,
-                children: [
-                  Image.asset(
-                    widget.restaurant.imageUrl,
-                    fit: BoxFit.cover,
-                  ),
-                  Positioned(
-                    top: 4.0,
-                    right: 4.0,
-                    child: IconButton(
-                      icon: Icon(
-                        _isFavorited
-                            ? Icons.favorite //
-                            : Icons.favorite_border,
-                      ),
-                      iconSize: 30.0,
-                      color: Colors.red[400],
-                      onPressed: () {
-                        setState(() {
-                          _isFavorited = !_isFavorited;
-                        });
-                      },
+          ),
+        );
+      },
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(8.0),
+              ),
+              child: AspectRatio(
+                aspectRatio: 2,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.asset(
+                      widget.restaurant.imageUrl,
+                      fit: BoxFit.cover,
                     ),
-                  ),
-                ],
+                    Positioned(
+                      top: 4.0,
+                      right: 4.0,
+                      child: IconButton(
+                        icon: Icon(
+                          _isFavorited
+                              ? Icons.favorite //
+                              : Icons.favorite_border,
+                        ),
+                        iconSize: 30.0,
+                        color: Colors.red[400],
+                        onPressed: () {
+                          setState(() {
+                            _isFavorited = !_isFavorited;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: Text(
-              widget.restaurant.name,
-              style: textTheme.titleSmall,
+            ListTile(
+              title: Text(
+                widget.restaurant.name,
+                style: textTheme.titleSmall,
+              ),
+              subtitle: Text(
+                widget.restaurant.attributes,
+                maxLines: 1,
+                style: textTheme.bodySmall,
+              ),
             ),
-            subtitle: Text(
-              widget.restaurant.attributes,
-              maxLines: 1,
-              style: textTheme.bodySmall,
-            ),
-            onTap: () {
-              // TODO: Push Restaurant Page
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
